b90539675a0d40d3c7a22bac2bd111a405e95ca4
mshetye <mayur.shetye@gmail.com>
2021-08-09 21:40:07 -0400

    Initial commit

---
+++ src/GFG2.java
@@ -0,0 +1,35 @@
+// Java program using flatMap() function
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+class GFG2 {
+    public static void main(String[] args)
+    {
+        // making the arraylist object of List of Integer
+        List<List<Integer> > number = new ArrayList<>();
+
+        // adding the elements to number arraylist
+        number.add(Arrays.asList(1, 2));
+        number.add(Arrays.asList(3, 4));
+        number.add(Arrays.asList(5, 6));
+        number.add(Arrays.asList(7, 8));
+
+        System.out.println("List of list-" + number);
+
+        // using flatmap() to flatten this list
+        List<Integer> flatList
+                = number.stream()
+                .flatMap(list -> list.stream())
+                .collect(Collectors.toList());
+        List list2 = number.stream()
+                .map(list -> list.stream())
+                .collect(Collectors.toList());
+
+        // printing the list
+        System.out.println("list2 "+ list2);
+
+        System.out.println("List generate by flatMap-"
+                + flatList);
+    }
+}
